2bb4e4380aeaeaaa2d33ea29a5768fba9802f207
iuliia <shevchuk.iuliia@gmail.com>
2018-12-07 12:56:10 +0200

    Binary Tree Inorder Traversal

---
+++ src/BinaryTreeInorderTraversal.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList();
+        traverseInorder(result, root);
+        return result;
+    }
+
+    private void traverseInorder(List<Integer> result, TreeNode root) {
+        if (root != null) {
+            traverseInorder(result, root.left);
+            result.add(root.val);
+            traverseInorder(result, root.right);
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int x) {
+        val = x;
+    }
+}
